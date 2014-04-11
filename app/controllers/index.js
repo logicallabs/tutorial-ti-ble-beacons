dcbbcf1201133c139b5ee6b99640c7614fbc85fc
Zsombor Papp <zsombor.papp@logicallabs.com>
2014-04-11 15:29:49 -0700

    Adding region monitoring
    This commit adds the primary functionality of the app, monitoring a region.
    
    A region is identified by a UUID, which in this case we store in the REGION_UUID
    variable. Monitoring a region means that we scan for beacons that belong to
    this region and notify the app whether the user is inside or outside the region.
    
    The startRegionMonitoring and stopRegionMonitoring functions are used
    to start and stop the scanning, and the regionStateUpdated event is used
    to signal changes in the region's state (inside or outside).

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,19 +3,57 @@
 
 var 
 	Beacons = Alloy.Globals.Beacons,
-	scanRunning = false
+	scanRunning = false,
+	beaconRegion,
+	REGION_UUID = 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'
 ;
 
+beaconRegion = Beacons.createBeaconRegion({
+					UUID: REGION_UUID,
+					identifier: 'Beacons'
+			});
+			
 Beacons.addEventListener('moduleReady', function() {
 	$.status.update('Beacons Module is ready!');
 });
 
+Beacons.addEventListener('regionStateUpdated', function(e) {
+	var stateStr;
+	
+	switch(e.state) {
+		case Beacons.REGION_STATE_UNKNOWN:
+			stateStr = 'unknown';
+			break;
+		case Beacons.REGION_STATE_INSIDE:
+			stateStr = 'inside.';
+			break;
+		case Beacons.REGION_STATE_OUTSIDE:
+			stateStr = 'outside.';
+			break;
+	}
+	$.status.update('Region state is now ' + stateStr);
+});
+
+function startScan() {
+	Beacons.startRegionMonitoring({
+		beaconRegion: beaconRegion
+	});
+}
+
+function stopScan() {
+	Beacons.stopRegionMonitoring({
+		beaconRegion: beaconRegion
+	});
+}
+
 function toggleScan() {
 	if (scanRunning) {
+		stopScan();
 		scanRunning = false;
 		$.toggleScanButton.title = 'Start scanning';
 		$.status.update('Scanning stopped');
 	} else {
+		startScan();
 		scanRunning = true;
 		$.toggleScanButton.title = 'Stop scanning';
 		$.status.update('Scanning started');
